9a68d33073bc9bab04baea9d5d1f8d9c450a315c
khansa-020 <khansaaraein@gmail.com>
2023-11-26 17:10:40 +0500

    navbar routing

--- Forntend/student/src/App.js
+++ Forntend/student/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+       <Nav/>
+<Routes>
+<Route path='/home' element={<h1>home</h1>} />
+<Route path='/about' element={<h1>about</h1>} />
+<Route path='/contactus' element={<h1>contact</h1>} />
+<Route path='/showcourses' element={<h1>course</h1>} />
+<Route path='/tools' element={<h1>tools</h1>} />
+<Route path='/signup' element={<h1>signup</h1>} />
+<Route path='/login' element={<h1>login</h1>} />
+
+</Routes>
+      </BrowserRouter>
     </div>
   );
 }
