81e53bbd653f623893f9372347c23da236ff2126
khansa-020 <khansaaraein@gmail.com>
2023-11-30 11:30:35 +0500

    connect mongoDB

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res)=>{
-  res.send("App i working")
-})
+const mongoose = require('mongoose');
+
+const DB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/FunZone", {
+    });
+    console.log("Connection Successful");
+  } catch (error) {
+    console.error("Connection Error:", error.message);
+  }
+};
+
+// Call the DB function to establish the connection
+DB();
+
+app.get("/", (req, res) => {
+  res.send("App is working");
+  console.log("Server is running");
+});
 
 app.listen(5000);
+
