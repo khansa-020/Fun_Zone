f581d4bd7afa116dffa8923780183fd63089385e
khansa-020 <khansaaraein@gmail.com>
2023-12-03 14:55:35 +0500

    test signup Api successfull

--- Server/db/config.js
+++ Server/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/Fun_Zone")
+mongoose.connect("mongodb://0.0.0.0:27017/Fun_Zone")
--- Server/index.js
+++ Server/index.js
@@ -3,22 +3,22 @@ const app = express();
 require("./db/config")
 const User= require("./User");
 
-app.use(express.json())
-app.post("/",(req, res) => {
-  //let user=new User(req.body);
-  //let result=await user.save();
-  res.send(req.body);
-  console.log("Server is running...");
-});
-
-app.listen(5000);
-
 // app.use(express.json())
-// app.post("/", async(req, res) => {
-//   let user=new User(req.body);
-//   let result=await user.save();
-//   res.send(result);
+// app.post("/",(req, res) => {
+//   //let user=new User(req.body);
+//   //let result=await user.save();
+//   res.send(req.body);
 //   console.log("Server is running...");
 // });
 
 // app.listen(5000);
+
+app.use(express.json())
+app.post("/", async(req, res) => {
+  let user=new User(req.body);
+  let result=await user.save();
+  res.send(result);
+  console.log("Server is running...");
+});
+
+app.listen(5000);
