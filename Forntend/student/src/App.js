4a732ffae34c44aa44594e75c6e71286e6dd4f11
khansa-020 <khansaaraein@gmail.com>
2024-03-16 12:15:01 +0500

    frontend add jerry Dashboard

--- Forntend/student/src/App.js
+++ Forntend/student/src/App.js
@@ -1,38 +1,47 @@
 import './App.css';
-// import Nav from './components/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import LandingPage from './pages/home/Home';
 import ContactUs from './pages/contact/ContactUs';
-import About from './pages/aboutus/About'
-import Tools from './pages/Tools/ArtisticTools'
-import Dashboard from './pages/Dashboard';
+import About from './pages/aboutus/About';
+import Tools from './pages/Tools/ArtisticTools';
 import EventCard from './pages/Events/Events';
+import Courses from "./pages/Courses/Courses";
 import SignUp from './pages/Signup/SignUp';
 import LogIn from './pages/Login/LogIn';
-import ErrorPage from './pages/ErrorPage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
 
+import Dashboard from './pages/Dashboard/StudentDashboard copy';
+import Profile from './pages/profile/Profile';
+import ApplyTrainer from './pages/ApplyForTrainer/TrainerRequest';
+import ChatRoom from './pages/Chat/ChatRoom';
+
+import SocialPage from './pages/Dashboard/socialpage/SocialPage';
+import ProfilePage from './pages/Dashboard/profilepage/ProfilePage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-       {/* <Nav/> */}
-<Routes>
-<Route path='/' element={<h1><LandingPage/></h1>} />
-<Route path='/about' element={<h1><About/></h1>} />
-<Route path='/contactus' element={<h1><ContactUs/></h1>} />
-<Route path='/events' element={<h1><EventCard/></h1>} />
-<Route path='/showcourses' element={<h1>course</h1>} />
-<Route path='/tools' element={<h1><Tools/></h1>} />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contactus' element={<ContactUs />} />
+          <Route path='/events' element={<EventCard />} />
+          <Route path='/showcourses' element={<Courses />} />
+          <Route path='/tools' element={<Tools />} />
 
-<Route path='/' element={<h1><LandingPage/></h1>} />
-<Route path='/dashboard' element={<h1><Dashboard/></h1>} />
-<Route path='/signup' element={<h1><SignUp/></h1>} />
-<Route path='/login' element={<h1><LogIn/></h1>} />
-<Route path="*" element={<ErrorPage/>} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/chatroom' element={<ChatRoom />} />
+          <Route path='/trainerRequest' element={<ApplyTrainer />} />
+          <Route path='/SocialPage' element={<SocialPage />} />
+          <Route path='/ProfilePage' element={<ProfilePage />} />
 
-</Routes>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
