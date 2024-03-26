b13802b8bcaa5aca6e224cef17af7b4e59c4c2ce
khansa-020 <khansaaraein@gmail.com>
2024-03-27 01:42:13 +0500

    added sidebar course  interface

---
+++ Forntend/trainer/src/pages/CreateCourses/Course Sidebar/CourseSidebar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '../../../Constants/button/button';
+import './CourseSidebar.css'; // Import CSS for styling
+
+
+
+const handleSubmit = () => {
+    // Handle button click (submit action)
+    console.log('Form submitted');
+  };
+
+
+function Sidebar({ selectedLink, handleLinkClick }) {
+  return (
+    <div className="course-sidebar">
+      <ul>
+        <li className={selectedLink === 'link1' ? 'active' : ''} onClick={() => handleLinkClick('link1')}>About Courses</li>
+        <li className={selectedLink === 'link2' ? 'active' : ''} onClick={() => handleLinkClick('link2')}>Course Structure</li>
+        <li className={selectedLink === 'link3' ? 'active' : ''} onClick={() => handleLinkClick('link3')}>Landing Page</li>
+        <li className={selectedLink === 'link4' ? 'active' : ''} onClick={() => handleLinkClick('link4')}>Create Content</li>
+        <li className={selectedLink === 'link5' ? 'active' : ''} onClick={() => handleLinkClick('link5')}>Pricing</li>
+        <li className={selectedLink === 'link6' ? 'active' : ''} onClick={() => handleLinkClick('link6')}>Course Messages</li>
+
+      </ul>
+      {/* Button for submission */}
+      <Button onClick={handleSubmit}>Submit for Review</Button>
+    </div>
+  );
+}
+
+export default Sidebar;
