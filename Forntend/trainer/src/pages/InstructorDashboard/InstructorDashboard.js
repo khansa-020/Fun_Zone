92d085ccb3592ebd53017611a402ccd0d7b0c8f6
khansa-020 <khansaaraein@gmail.com>
2024-04-08 22:37:10 +0500

    Dashboard Links interfaces

--- Forntend/trainer/src/pages/InstructorDashboard/InstructorDashboard.js
+++ Forntend/trainer/src/pages/InstructorDashboard/InstructorDashboard.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Sidebar from './InstructorSidebar'; 
+import Logo from '../.././Constants/Logo/logo';
 // import './InstructorDashboard.css'; 
 
 const InstructorDashboard = () => {
   const links = [
     { url: '/InstructorProfile', label: 'Profile' },
     { url: '/CreateCourse', label: 'Create Course' },
-    { url: '/Performance ', label: 'Performance ' },
+    { url: '/Performance ', label: 'Performance' },
     { url: '/CourseLandingPage', label: 'Manage Courses' },
     { url: '/MyCourses', label: 'My Courses' },
-    // { url: '/Chat', label: 'Chat' }
+    { url: '/Chat', label: 'Chat' }
   ];
 
   
   return (
     <div className="instructor-dashboard">
+
+      <div className='logo-comon'>
+        <Logo showStepCount={false} /> 
+      </div>
+  
+
 
       <Sidebar links={links}  />
 
--- Forntend/trainer/src/pages/InstructorDashboard/InstructorSidebar.js
+++ Forntend/trainer/src/pages/InstructorDashboard/InstructorSidebar.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'; // Icon for the hamburger menu
+import { faUser, faBook, faChartColumn, faCog, faTasks, faComment } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
-// import './Sidebar.css';
+import Button from '../.././Constants/button/button';
+import './InstructorSidebar.css';
 
 const Sidebar = ({ links }) => {
+  const [isOpen, setIsOpen] = useState(false); // State to track if sidebar is open
+
+  const iconMap = {
+    Profile: faUser,
+    'Create Course': faBook,
+    'Performance': faChartColumn,
+    'Manage Courses': faCog,
+    'My Courses': faTasks,
+    Chat: faComment
+  };
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen); // Toggle the state of the sidebar
+  };
+
+  const handleSubmit = () => {
+    // Handle button click (submit action)
+    console.log('Form submitted');
+  };
+
   return (
     <div id="body">
-
-      <div id="sidebar">
-        {links.map((link, index) => (
-          <a key={index} href={link.url}> {link.label} </a>
-        ))}
+      <div className={`dashboard-sidebar ${isOpen ? 'open' : ''}`}>
+        <div className="menu-toggle" onClick={toggleSidebar}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <ul>
+          {links.map((link, index) => (
+            <li key={index}>
+              <a href={link.url} onClick={toggleSidebar}>
+                <FontAwesomeIcon className='icons' icon={iconMap[link.label]} />
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <Button onClick={handleSubmit}>Submit for Review</Button>
       </div>
-
-
-      
     </div>
   );
 };
@@ -25,7 +56,6 @@ Sidebar.propTypes = {
       label: PropTypes.string.isRequired,
     })
   ).isRequired,
-  onToggle: PropTypes.func.isRequired,
 };
 
 export default Sidebar;
