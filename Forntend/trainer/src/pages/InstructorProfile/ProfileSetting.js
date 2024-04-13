0ea569f17b85ea05a4865093a453ced3397a854c
khansa-020 <khansaaraein@gmail.com>
2024-04-14 03:32:04 +0500

    Profile info_pic interfaces

--- Forntend/trainer/src/pages/InstructorProfile/ProfileSetting.js
+++ Forntend/trainer/src/pages/InstructorProfile/ProfileSetting.js
@@ -3,10 +3,8 @@ import React from 'react';
 const ProfileSettings = () => {
   return (
     <div>
-      <h2>Profile Settings</h2>
       <p>Here you can update your profile settings, such as language, website, social media links, etc.</p>
-      {/* Add your profile settings update functionality here */}
-    </div>
+=    </div>
   );
 };
 
